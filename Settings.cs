28f0fa7711476b2d200f0a4fb412e29050c3d4be
Thomas Jepp <tom@tomjepp.co.uk>
2016-08-16 03:35:50 +0100

    Add support for platform detection, and a settings file for forcing paths.

---
+++ Settings.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ThomasJepp.SaintsRow
+{
+    public class Settings
+    {
+        private static string ThisAssemblyPath
+        {
+            get
+            {
+                return Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            }
+        }
+
+
+        public string SaintsRow2Path { get; private set; }
+        public string SaintsRowTheThirdPath { get; private set; }
+        public string SaintsRowIVPath { get; private set; }
+        public string SaintsRowGatOutOfHellPath { get; private set; }
+
+        public Settings()
+        {
+            JObject json = null;
+
+            using (StreamReader sr = new StreamReader(Path.Combine(ThisAssemblyPath, "settings.json")))
+            {
+                using (JsonReader reader = new JsonTextReader(sr))
+                {
+                    JToken token = JToken.ReadFrom(reader);
+                    json = token as JObject;
+                }
+            }
+
+            if (json == null)
+            {
+                throw new InvalidDataException("Could not parse settings.json!");
+            }
+
+            JObject paths = json["paths"] as JObject;
+            if (paths == null)
+            {
+                throw new InvalidDataException("Could not find a 'paths' object in settings.json.");
+            }
+
+            SaintsRow2Path = paths["sr2"].Value<string>();
+            SaintsRowTheThirdPath = paths["srtt"].Value<string>();
+            SaintsRowIVPath = paths["sriv"].Value<string>();
+            SaintsRowGatOutOfHellPath = paths["srgooh"].Value<string>();
+        }
+    }
+}
--- Utility.cs
+++ Utility.cs
@@ -8,12 +8,44 @@ namespace ThomasJepp.SaintsRow
 {
     public static class Utility
     {
+        private static Platform DetectPlatform()
+        {
+            switch (Environment.OSVersion.Platform)
+            {
+                case PlatformID.Unix:
+                    // Determine if Unix is Mac OS or Linux.
+                    if (Directory.Exists("/Applications") && Directory.Exists("/System"))
+                        return Platform.macOS;
+                    else
+                        return Platform.Linux;
+
+                case PlatformID.MacOSX:
+                    return Platform.macOS;
+
+                default:
+                    return Platform.Windows;
+            }
+        }
+
         private static string GetRegistryEntry(string key, string value)
         {
             return (string)Microsoft.Win32.Registry.GetValue(key, value, null);
         }
 
         public static string GetSteamPath()
+        {
+            Platform platform = DetectPlatform();
+            switch (platform)
+            {
+                case Platform.Windows:
+                    return GetSteamPathWindows();
+
+                default:
+                    return null;
+            }
+        }
+
+        private static string GetSteamPathWindows()
         {
             string steamPath = GetRegistryEntry(@"HKEY_LOCAL_MACHINE\SOFTWARE\Valve\Steam", "InstallPath");
             if (steamPath == null)
@@ -24,6 +56,31 @@ namespace ThomasJepp.SaintsRow
 
         public static string GetGamePath(GameSteamID gameId)
         {
+            Settings settings = new Settings();
+
+            switch (gameId)
+            {
+                case GameSteamID.SaintsRow2:
+                    if (settings.SaintsRow2Path != null)
+                        return settings.SaintsRow2Path;
+                    break;
+
+                case GameSteamID.SaintsRowTheThird:
+                    if (settings.SaintsRowTheThirdPath != null)
+                        return settings.SaintsRowTheThirdPath;
+                    break;
+
+                case GameSteamID.SaintsRowIV:
+                    if (settings.SaintsRowIVPath != null)
+                        return settings.SaintsRowIVPath;
+                    break;
+
+                case GameSteamID.SaintsRowGatOutOfHell:
+                    if (settings.SaintsRowGatOutOfHellPath != null)
+                        return settings.SaintsRowGatOutOfHellPath;
+                    break;
+            }
+
             int id = (int)gameId;
 
             string steamPath = GetSteamPath();

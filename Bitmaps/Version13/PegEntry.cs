4ba67084c67fc2f9ad1433321d41cd560b35bfdc
Thomas Jepp <tom@tomjepp.co.uk>
2016-09-23 15:49:35 +0100

    Add a function to get bitmap data.

--- Bitmaps/Version13/PegEntry.cs
+++ Bitmaps/Version13/PegEntry.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,21 @@ namespace ThomasJepp.SaintsRow.Bitmaps.Version13
 
         public PegEntry()
         {
+        }
+
+        public byte[] GetData(Stream s)
+        {
+            s.Seek(Data.DataPtr.Value, SeekOrigin.Begin);
+            long length = Data.FrameSize * Data.NumFrames;
+            byte[] data = new byte[length];
+
+            long read = 0;
+            while (read < length)
+            {
+                read += s.Read(data, (int)read, (int)(length - read));
+            }
+
+            return data;
         }
     }
 }
